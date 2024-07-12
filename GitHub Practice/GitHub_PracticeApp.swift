ce656ea39dd7ea611657f6e7993311317a87db48
Luna <gonzalezgonzalezl@catlin.edu>
2024-07-12 12:02:30 -0700

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by Luna Gonzalez on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
